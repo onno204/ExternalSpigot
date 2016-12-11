e69d54ff9f240cbf761433521ae389bf64953a5e
onno204 <restafval@hotmail.nl>
2016-12-11 23:27:31 +0100

    Commenting

--- RunMultiple Servers/src/onno204ServerRunner/Utils.java
+++ RunMultiple Servers/src/onno204ServerRunner/Utils.java
@@ -1,28 +1,30 @@
 package onno204ServerRunner;
 
 import java.io.BufferedReader;
-import java.io.Writer;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Utils {
-
+	//Output every String
 	public static void Listnen(InputStream s, String Programname){
+		//Buffer reader to read.
 		BufferedReader in = new BufferedReader(new InputStreamReader(s));
 		String line = null;
 		try {
+			//Output to console
 			while((line = in.readLine()) != null) {
 			  System.out.println(Programname + "> " + line);
 			}
 		} catch (IOException e) { }
 	}
+	//Error reader, Same as above
 	public static void error(InputStream s, String Programname){
 		BufferedReader in = new BufferedReader(new InputStreamReader(s));
 		String line = null;
@@ -32,19 +34,25 @@ public class Utils {
 			}
 		} catch (IOException e) { }
 	}
+	
+	//Run (Called from SErver1T and Server2T)
 	public static void Run(String file, String Programname, ServerTypes server){
         try {
+        	//Get Server working dir
         	File dir = new File(System.getProperty("user.dir") + "/" + Programname + "/");
         	if(!dir.exists()) {
         		dir.mkdir();
         	}
+        	//Start procces
 			Process proc = null;
 			ProcessBuilder pb = new ProcessBuilder("java", "-jar", dir.getAbsolutePath() + "/" + file);
 			pb.directory(dir);
 			proc = pb.start();
+			//If ServerTpe = Bungeecord Set Bungeecord streams and writters
 			if(server == ServerTypes.BungeeCord){
 				BungeeCordStream = proc.getOutputStream();
 				ReCreateWritters();
+				//List for the /stop
 				ProcST.put(proc, ServerTypes.BungeeCord);
 				System.out.println("BungeeCord proccess Created");
 			}else if(server == ServerTypes.Lobby){
@@ -54,18 +62,18 @@ public class Utils {
 				System.out.println("Lobby proccess Created");
 			}
 			System.out.println("Creating listeners");
+			//Logging to console
 			InputStream ListenS = proc.getInputStream();
+			//Separete thread so we can also listen to the error's
 			Thread t = new Thread(new Runnable() { public void run() { Listnen(ListenS, Programname ); } });
 			t.start();
 			System.out.println(pb.directory());
 			list.add(proc);
-			//InputStreams.put(Programname, proc.getOutputStream());
-			//Listnen( proc.getInputStream(), Programname );
 			error(proc.getErrorStream(), Programname);
 			
 		} catch (IOException e) { System.out.println(e.toString()); }
 	}
-	
+	//Recreate writters, Set the writters when RunProcces() is called
 	public static void ReCreateWritters(){
 		System.out.println("ReCreating!");
 		try {
@@ -82,8 +90,9 @@ public class Utils {
 		} catch (Exception e) { System.out.println(e.toString()); }
 	}
 	
+	//Write to the console of spigot/bungee
 	public static void Write(String s, ServerTypes type){
-		
+		//I didn't know it any more and delete the old code :|
 		if(type == ServerTypes.BungeeCord){
 			
 		}else if(type == ServerTypes.Lobby){
--- RunMultiple Servers/src/onno204ServerRunner/main.java
+++ RunMultiple Servers/src/onno204ServerRunner/main.java
@@ -6,12 +6,15 @@ public class main {
 	
 	static int interval;
 	
+	//start
 	public static void main(String[] args) {
+		//Start Spigot
 		Runnable r = new Server1T();
 		new Thread(r).start();
-		
+		//STart bungeecord
 		r = new Server2T();
 		new Thread(r).start();
+		//Start Command Listener
 		KeyListener();
 		return;
 		/*
@@ -28,16 +31,16 @@ public class main {
 		Scanner reader = new Scanner(System.in);
 		while(true){
 			try { 
+				//Ask for command
 				System.out.println("Command: ");
 				String cmd = reader.nextLine();
-				System.out.println("Command: " + cmd);
-				//System.in.read();
+				System.out.println("Command: " + cmd); 
 				
+				//Check's if it's a need to stop
 				if(cmd.toLowerCase().startsWith("stop")){
-					Utils.BungeecordWritter.write("end");
-					Utils.LobbyWritter.write("stop");
-					Utils.LobbyWritter.close();
-					Utils.ReCreateWritters();
+					Utils.Write("stop", ServerTypes.BungeeCord);
+					Utils.Write("stop", ServerTypes.Lobby);
+					//Couting so the server has gotten a time to close
 					int x = 1;
 					for(int i=0; i < Utils.list.size(); i++){
 						System.out.println("Stopping server " + i);
@@ -47,16 +50,19 @@ public class main {
 							x++;
 							Thread.sleep(2);
 							if(x > 10000){
+								//Destroy the porcess after 20000 mil. seconds
 								proc.destroyForcibly();
 							}
 						}
-						//proc.destroyForcibly();
 					}
 					break;
 				}
-				
+				//Command sender
+				//Example: LobbeHelp  > Executes Help
 				if(cmd.startsWith("Lobby") ){
+					//Write's to stream (Not working)
 					Utils.Write(cmd.replaceFirst("Lobby", ""), ServerTypes.Lobby);
+					//Message to console it has been send
 					System.out.println("Sending '" + cmd.replaceFirst("Lobby", "") + "' to Lobby.");
 				}else if(cmd.startsWith("Bungeecord") ){
 					Utils.Write(cmd.replaceFirst("Bungeecord", ""), ServerTypes.BungeeCord);
